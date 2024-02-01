eefc49743f191deac21bc18991c16382dd0713b8
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2024-02-01 16:57:56 -0300

    Project created

---
+++ src/test/java/com/lalsz/coursemongodb/CoursemongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalsz.coursemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
