5e64d3338bf4a72d613c7c13f5c04d6f41548a74
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2024-02-02 16:31:34 -0300

    Consultation with various criteria

--- src/main/java/com/lalsz/coursemongodb/services/PostService.java
+++ src/main/java/com/lalsz/coursemongodb/services/PostService.java
@@ -7,6 +7,7 @@ import com.lalsz.coursemongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +28,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return postRepository.findByTitleContainingIgnoreCase(text);
+    }
+
+    public List<Post> search(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.search(text, minDate, maxDate);
     }
 }
