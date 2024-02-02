d2ef4f56232f54a3054df7373404dfe0ab1a949a
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2024-02-02 15:24:02 -0300

    Adding comments to posts

--- src/main/java/com/lalsz/coursemongodb/config/Instantiation.java
+++ src/main/java/com/lalsz/coursemongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.lalsz.coursemongodb.config;
 import com.lalsz.coursemongodb.domain.Post;
 import com.lalsz.coursemongodb.domain.User;
 import com.lalsz.coursemongodb.dto.AuthorDTO;
+import com.lalsz.coursemongodb.dto.CommentDTO;
 import com.lalsz.coursemongodb.repository.PostRepository;
 import com.lalsz.coursemongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,10 +42,19 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Broncos", "Vou jogar nos Broncos", new AuthorDTO(caleb));
         Post post2 = new Post(null, sdf.parse("22/03/2018"), "Partiu Commanders", "Vou jogar nos Commanders", new AuthorDTO(maye));
 
+        CommentDTO comm1 = new CommentDTO("Let's ride! ", sdf.parse("23/03/2018"), new AuthorDTO(daniels));
+        CommentDTO comm2 = new CommentDTO("Boa sorte! ", sdf.parse("23/03/2018"), new AuthorDTO(maye));
+        CommentDTO comm3 = new CommentDTO("Let's go! ", sdf.parse("23/03/2018"), new AuthorDTO(daniels));
+
+        post1.getComments().addAll(Arrays.asList(comm1, comm2));
+        post2.getComments().add(comm3);
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         caleb.getPosts().add(post1);
         maye.getPosts().add(post2);
         userRepository.saveAll(Arrays.asList(caleb, maye));
+
+
     }
 }
--- src/main/java/com/lalsz/coursemongodb/resources/PostResource.java
+++ src/main/java/com/lalsz/coursemongodb/resources/PostResource.java
@@ -1,18 +1,10 @@
 package com.lalsz.coursemongodb.resources;
 
 import com.lalsz.coursemongodb.domain.Post;
-import com.lalsz.coursemongodb.domain.User;
-import com.lalsz.coursemongodb.dto.UserDTO;
 import com.lalsz.coursemongodb.services.PostService;
-import com.lalsz.coursemongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
