bc6c22742077dc52bf91a4073e3531df6fa3de57
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2024-02-02 14:07:45 -0300

    user update with PUT

--- src/main/java/com/lalsz/coursemongodb/services/UserService.java
+++ src/main/java/com/lalsz/coursemongodb/services/UserService.java
@@ -37,6 +37,17 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+    public User update(User obj) {
+        User newObj = userRepository.findById(obj.getId()).get();
+        updateData(newObj, obj);
+        return userRepository.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
