9e97d2fddf9a2997e799b692c68993fb91be058f
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2024-02-02 15:02:56 -0300

    Endpoint to return a user's posts

--- src/main/java/com/lalsz/coursemongodb/resources/UserResource.java
+++ src/main/java/com/lalsz/coursemongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.lalsz.coursemongodb.resources;
 
+import com.lalsz.coursemongodb.domain.Post;
 import com.lalsz.coursemongodb.domain.User;
 import com.lalsz.coursemongodb.dto.UserDTO;
 import com.lalsz.coursemongodb.services.UserService;
@@ -53,5 +54,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
